eb6142565a43944a090353408928d800755523b9
ALAHMED	ZAHRAA <s202344670@ccm.kfupm.edu.sa>
2026-04-15 13:33:21 +0300

    TASK DB-1

--- 7-2-restful-api/server/db.js
+++ 7-2-restful-api/server/db.js
@@ -5,11 +5,19 @@
  *    - Export connectDB() that connects Mongoose using MONGO_URL
  *    - Log success; throw on failure
  */
+
 import mongoose from "mongoose";
 
-export async function connectDB(url) {
+export async function connectDB() {
+  const mongoUrl = process.env.MONGO_URL;
+  if (!mongoUrl) {
+    const err = new Error("MONGO_URL is not defined");
+    console.error("Connection error:", err.message);
+    throw err;
+  }
+
   try {
-    await mongoose.connect(url);
+    await mongoose.connect(mongoUrl);
     console.log("[DB] Mongo connected");
   } catch (err) {
     console.error("Connection error:", err.message);
