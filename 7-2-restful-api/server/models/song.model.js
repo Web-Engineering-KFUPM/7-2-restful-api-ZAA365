25348fe986a310fd9706dedde8949d7730f9a00a
ALAHMED	ZAHRAA <s202344670@ccm.kfupm.edu.sa>
2026-04-15 13:06:53 +0300

    TASK 2 — Create Schema & Model

--- 7-2-restful-api/server/models/song.model.js
+++ 7-2-restful-api/server/models/song.model.js
@@ -1,29 +1,11 @@
 import mongoose from "mongoose";
 
 // db schema
+const songSchema = new mongoose.Schema({
+title: { type: String, required: true, trim: true },
+artist: { type: String, required: true, trim: true },
+year: { type: Number, min: 1900, max: 2100 }
+}, { timestamps: true });
 
 export const Song = mongoose.model("Song", songSchema);
-/** =================================================================
- *  TASK 2 — Create Schema & Model (file: server/models/song.model.js)
- *  =================================================================
- *  Goal:
- *    - Define a Song schema with fields:
- *        title (String, required)
- *        artist (String, required)
- *        year (Number)
- *    - Export a Mongoose model named "Song".
- *
- *  Example:
- *    name: { type: String, required: true, trim: true }
- *      Note: trim is used to remove the extra spaces automatically.
- * 
- *  Syntax hint:
- * 
-      const songSchema = new mongoose.Schema({
-        title:  { type: _______, required: _____, trim: _____ },
-        artist: { type: _______, required: _____, trim: _____ },
-        year:   { type: _______, min: _____, max: _____ }
-      }, { timestamps: _____ });
-
-      const Song = mongoose.model("Song", songSchema);
- */
+export default Song;
